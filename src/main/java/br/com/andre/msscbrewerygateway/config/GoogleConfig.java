c91605eb783f8d9bfaad78a0c11ea7fc78190c0d
André Luiz <andre.andrade@vsoft.com.br>
2021-01-25 12:02:51 -0300

    Example configuring spring cloud gateway to google. Closes #1

---
+++ src/main/java/br/com/andre/msscbrewerygateway/config/GoogleConfig.java
@@ -0,0 +1,21 @@
+package br.com.andre.msscbrewerygateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile("google")
+@Configuration
+public class GoogleConfig {
+
+    @Bean
+    public RouteLocator googleConfigGateway(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/googlesearch")
+                        .filters(f -> f.rewritePath("/googlesearch(?<segment>/?.*)", "/${segment}"))
+                        .uri("http://google.com"))
+                .build();
+    }
+}
