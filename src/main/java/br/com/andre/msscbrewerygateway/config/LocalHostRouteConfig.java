adec504aeb7d68ec308484ceb6f29377be49ed71
André Luiz <andre.andrade@vsoft.com.br>
2021-01-26 16:03:33 -0300

    Adding Route for Orders. Closes #3

--- src/main/java/br/com/andre/msscbrewerygateway/config/LocalHostRouteConfig.java
+++ src/main/java/br/com/andre/msscbrewerygateway/config/LocalHostRouteConfig.java
@@ -16,6 +16,10 @@ public class LocalHostRouteConfig {
                         "/api/v1/beer/*",
                         "/api/v1/beerUpc/*")
                         .uri("http://localhost:8080"))
+                .route(r -> r.path(
+                        "/api/v1/customers",
+                        "/api/v1/customers/**")
+                .uri("http://localhost:8081"))
                 .build();
     }
 
