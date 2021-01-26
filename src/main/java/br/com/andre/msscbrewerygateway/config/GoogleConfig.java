12c1ec68835f979ecc8a69e3c038151f7e6c32d4
André Luiz <andre.andrade@vsoft.com.br>
2021-01-26 14:46:24 -0300

    Adding local host routes. Closes #2

--- src/main/java/br/com/andre/msscbrewerygateway/config/GoogleConfig.java
+++ src/main/java/br/com/andre/msscbrewerygateway/config/GoogleConfig.java
@@ -6,8 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-@Profile("google")
-@Configuration
+//@Profile("google")
+//@Configuration
 public class GoogleConfig {
 
     @Bean
